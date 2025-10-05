3beb4e93dd207c7fa719438cc0bc914264d311b7
lucianochimale8 <kchimale@gmail.com>
2025-10-05 11:15:56 -0300

    Camera-Follow-Player: script con offSet al player para que la camara siga al mismo

---
+++ Assets/Prefabs/Player/Model/CameraFollowPlayer.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    private Vector3 offSet;
+    private PlayerMovement playerMovement;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        offSet = new Vector3(0, 1, -5);
+        playerMovement = FindObjectOfType<PlayerMovement>();
+        
+    }
+
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        gameObject.transform.position = playerMovement.transform.position + offSet;
+    }
+}
